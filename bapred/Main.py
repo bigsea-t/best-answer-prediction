5e68ca043b8ec4784b0d0d1506cb96c95011445e
seataK <297.1951@gmail.com>
2016-02-24 13:35:22 -0800

    LSA with outer product of QA LSA with outer product of QA

--- bapred/Main.py
+++ bapred/Main.py
@@ -18,7 +18,30 @@ def dump_model(model, name, data_dir):
     with open('{}{}.pkl'.format(data_dir, name), 'wb') as f:
         pickle.dump(model, f)
 
+
+def generate_similarity(Xq, Xa, n_ans, svd):
+    Zq = svd.transform(Xq)
+    Za = svd.transform(Xa)
+    A = np.repeat(Zq, n_ans, axis=0)
+    B = Za
+    simZ = np.sum(A * B, axis=1) / (np.sqrt(np.sum(A * A, axis=1)) * np.sqrt(np.sum(B * B, axis=1)) + np.finfo(float).eps)
+    return simZ[:, np.newaxis]
+
+def generate_corr(Xq, Xa, n_ans, svd):
+    Zq = svd.transform(Xq)
+    Za = svd.transform(Xa)
+    A = np.repeat(Zq, n_ans, axis=0)
+    B = Za
+
+    corr = []
+    for a, b in zip(A,B):
+        corr.append(np.outer(a,b).flatten())
+
+    return np.vstack(corr)
+
+
 if __name__ == '__main__':
+    print('Main...')
     if len(sys.argv) != 3:
         raise Exception('Usage: python Main.py <data_dir> <ML/Heuristic>[0/1]')
     data_dir = sys.argv[1]
@@ -47,28 +70,30 @@ if __name__ == '__main__':
             print()
             
     else:
-        questions, answers, scores = get_raw_data_score(data_dir, n_ans, n_files=100)
-        Xq, Xa, feature_names = transform_raw_data(questions, answers)
+        questions, answers, scores = get_raw_data_score(data_dir, n_ans, n_files=102)
+        print('Data Loaded.')
+
+        Xq, Xa, feature_names = transform_raw_data(questions, answers, remove_tag=True)
         feature_names.append('correlation qa')
+        print('Data transformed')
 
         Y = reguralize_score(scores, n_ans)
 
         Xqtr, Xatr, Ytr, Xqte, Xate, Yte = split_data(Xq, Xa, Y, n_ans)
+        print('Data Splitted')
 
-        svd = TruncatedSVD(n_components=100)
+        svd = TruncatedSVD(n_components=30)
         Xqatr = sp.sparse.vstack((Xqtr, Xatr))
         svd.fit(Xqatr)
+        print('svd fitted')
 
-        Zqtr = svd.transform(Xqtr)
-        Zatr = svd.transform(Xatr)
-        A = np.repeat(Zqtr, n_ans, axis=0)
-        B = Zatr
-        simZtr = np.sum(A * B, axis=1) / (np.sqrt(np.sum(A * A, axis=1)) * np.sqrt(np.sum(B * B, axis=1)) + np.finfo(float).eps)
-
-        Zqte = svd.transform(Xqte)
-        Zate = svd.transform(Xate)
-        simZte = np.sum(np.repeat(Zqte, n_ans, axis=0) * Zate, axis=1)
-
+        # simZtr = generate_similarity(Xqtr, Xatr, n_ans, svd)
+        # simZte = generate_similarity(Xqte, Xate, n_ans, svd)
+        print('Xqatr', Xqatr.shape)
+        simZtr = generate_corr(Xqtr, Xatr, n_ans, svd)
+        simZte = generate_corr(Xqte, Xate, n_ans, svd)
+        print('simZtr.shape', simZtr.shape)
+        print('Sim generated')
 
         models = {
             'Logistic Regression': LogisticRegressionWrapper(n_ans=n_ans, penalty='l2', fit_intercept='True'),
@@ -81,13 +106,17 @@ if __name__ == '__main__':
         # NOTE: Non-Linear SVM is not scalable
         # On top of that, Linear SVM is supposed to be enough for this high dimensional features
 
-        use_cach = True
+        use_cach = False
+
+        print(Xatr.shape)
+        Xtr = sp.sparse.hstack((Xatr, simZtr))
+        print(Xtr.shape)
+        Xte = sp.sparse.hstack((Xate, simZte))
 
         for name, model in models.items():
-            # Xtr = sp.sparse.hstack((Xatr, simZtr[:, np.newaxis]))
-            # Xte = sp.sparse.hstack((Xate, simZte[:, np.newaxis]))
-            Xtr = Xatr
-            Xte = Xate
+
+            # Xtr = Xatr
+            # Xte = Xate
 
             if use_cach:
                 model = load_model(name, data_dir)
@@ -106,21 +135,6 @@ if __name__ == '__main__':
             print('test set accuracy:    ', prec_at_1(score_ans_te, Yte, n_ans))
             print('high score feature:\n', sorted_features[:20])
             print('low score feature :\n', sorted_features[-20:])
-            print()
-
-            model2 = LogisticRegressionWrapper(n_ans=n_ans, penalty='l2', fit_intercept='True')
-            Xtr2 = np.hstack((score_ans_tr[:, np.newaxis], simZtr[:, np.newaxis]))
-            Xte2 = np.hstack((score_ans_te[:, np.newaxis], simZte[:, np.newaxis]))
-
-            model2.fit(Xtr2, Ytr)
-
-            score_te = model2.predict_score(Xte2)
-            score_tr = model2.predict_score(Xtr2)
-    
-            print('- merge qa correlation -')
-            print('training set accuracy:', prec_at_1(score_tr, Ytr, n_ans))
-            print('test set accuracy:    ', prec_at_1(score_te, Yte, n_ans))
-            print('coef', model2.coef_)
             print()
 
     print('done')
